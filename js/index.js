5d06716f7a96ed3be0b0b4be7d985a2a1096e3b0
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-10-04 15:07:58 +0300

    set up

--- js/index.js
+++ js/index.js
@@ -1,34 +1,24 @@
-import { key } from './api_key';
+document.getElementById('submit-button').addEventListener("click", submitForm, false);
 
 function submitForm(){
 
-    console.log(key)
+    // console.log(api_key.api_key)
 
     var search = document.getElementById("search").value;
     
-    fetch("https://restcountries.eu/rest/v2/all")
+    fetch("https://restcountries.com/v3.1/all") //from https://restcountries.com/
     .then((result)=> result.json())
     .then((data) =>{
     
-        var countryName, capital, region, timeZone, mainLanguage ;
+        console.log(data)
+        var countryName, capital, region, timeZone, language, currency ;
 
-        for (let i = 0; i < data.length; i++) {   
-            if(data[i].name.toLowerCase() == search.toLowerCase()){
-               countryName = data[i].name;
-               capital =  data[i].capital;
-               region = data[i].region;
-
-               document.getElementById("result").innerHTML =
+        document.getElementById("result").innerHTML = 
+               
                `
-                <p>${countryName}</p>
-                <p>capital : ${capital}</p>
-                <p>region : ${region}</p>
+               <h4>result<h4>
+               <iframe src="https://restcountries.com" style=" width: 60%; height: 500px;" ></iframe>
                `
-            }
-        }
-
-        if(!countryName){
-            alert("there is no country with the name : " + search)
-        }
+               ;         
     })
 }
