2214f62690c8cdcaba34354db8f28ff595a57910
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-10-07 13:27:33 +0300

    here map (not finished)

--- js/index.js
+++ js/index.js
@@ -1,54 +1,68 @@
+var input = document.getElementById("search")
+var divResult = document.getElementById("result")
+var submit = document.getElementById('submit-button')
 
+submit.addEventListener("click", submitForm, false);
 
-document.getElementById('submit-button').addEventListener("click", submitForm, false);
+input.addEventListener("keyup", function(event) { 
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("submit-button").click();
+  }
+});
+
+// loadJs('https://unpkg.com/leaflet@1.4.0/dist/leaflet.js')
+loadJs('http://js.api.here.com/v3/3.0/mapsjs-core.js')
+loadJs('http://js.api.here.com/v3/3.0/mapsjs-service.js')
+
+function loadJs(link){
+  var script = document.createElement('script');
+  script.src = link;
+  script.type = 'text/javascript'
+}
 
 function submitForm(){
 
-     console.log()
-
-    var search = document.getElementById("search").value;
+  var search = input.value;
     
-    fetch("https://restcountries.com/v3.1/all") //from https://restcountries.com/
-    .then((result)=> result.json())
-    .then((data) =>{
+  fetch("https://restcountries.com/v3.1/all") //from https://restcountries.com/
+   .then((result)=> result.json())
+      .then((data) =>{
      
         var countryName, capital, region, currency, latlng ;
 
          for (let index = 0; index < data.length; index++) {
            
-           if(search.toLowerCase().trim() == data[index]['name']['common'].toLowerCase().trim()){
+            if(search.toLowerCase().trim() == data[index]['name']['common'].toLowerCase().trim()){
 
-            var indexInfo = data[index]
+              var indexInfo = data[index]
 
-            countryName = indexInfo['name']['common']
-            capital = indexInfo['capital'][0]
-            region = indexInfo['region']
-            latlng = [indexInfo['latlng'][0], indexInfo['latlng'][1]]
+              countryName = indexInfo['name']['common']
+              capital = indexInfo['capital'][0]
+              region = indexInfo['region']
+              latlng = [indexInfo['latlng'][0], indexInfo['latlng'][1]]
 
-            console.log(latlng[0] + ", " + latlng[1])
-            document.getElementById("result").innerHTML =               
-            `
-            <h3>${countryName}</h3>
-            <h4>capital : ${capital}</h4>
-            <h4>region : ${region}</h4>
-            <iframe id="map" style=" width: 60%; height: 500px;" ></iframe> 
-            `
-            map(latlng[1], latlng[0])
-           }
-         }      
-    })
+              divResult.style.border = "thick solid #000000"
+              divResult.style.padding = "10px"
+              divResult.style.borderRadius = "20px"
+            
+              divResult.innerHTML =               
+              `
+              <h3>${countryName}</h3>
+              <h4>capital : ${capital}</h4>
+              <h4>region : ${region}</h4>
+              <div style="margin-left: 20%; width: 60%; height: 500px;" id="mapContainer"></div>
+              `
+              mapInit(latlng[0], latlng[1])
+            }
+          }      
+      })
 }
 
-//src="https://restcountries.com/v3.1/all"
-
-function mapInit(lng = 0, lat = 0){
-
-    var platform = new H.service.Platform({
-        'apikey': 'WxL_1X5zBveqL0eEG41NsVRpgb2bdBzLDPyiigqESM8'
-      });
-    
-      var maptypes = platform.createDefaultLayers();
-    
-      var map = new H.Map(document.getElementById('map'),
-      maptypes.vector.normal.map,{zoom: 10, center: { lng: lng, lat: lat }});
-}
+ function mapInit(varLat, varLng){
+  var platform = new H.service.Platform({'apikey': 'FQaJSL8jNzYUI4_3cuA96LrSd7tmC8UtcDX_-rrdvz0'});    
+  var map = new H.Map(
+      document.getElementById('mapContainer'),
+      platform.createDefaultLayers().vector.normal.map,{zoom: 20, center: { lat: varLat, lng: varLng }});
+ }
+ 
