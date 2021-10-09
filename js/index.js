ee08ee693c27a6f491a8c2373ea115aaaf60a6fb
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-10-09 22:03:30 +0300

    dark mode added

--- js/index.js
+++ js/index.js
@@ -3,7 +3,23 @@ var input = document.getElementById("search")
 var divResult = document.getElementById("result")
 var submit = document.getElementById('submit-button')
 
-// this api key is expired
+const options = {
+  bottom: '24px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#cfcccc',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
 var platform = new H.service.Platform({'apikey': 'FQaJSL8jNzYUI4_3cuA96LrSd7tmC8UtcDX_-rrdvz0'});    
 
 submit.addEventListener("click", submitForm, false);
